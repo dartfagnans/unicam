edefadbfbe44c08e08057329517bd4d14982e0c1
dartfagnans <giuseppe.fagnani@studenti.unicam.it>
2018-05-15 15:22:08 +0200

    modifiche readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Applicazione Web di prova
 
 Author: Giuseppe Fagnani (dartfagnans)
+
+Istruzioni per l'avvio: digitare nodemon app.js e collegarsi in localhost sulla porta 3000
--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-var express = require ('express');
-var app = express();
+var express = require('express');
+const app = express();
+
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var cookieSession = require('cookie-session');
+var cookieParser = require('cookie-parser');
+
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({
+    name: 'session',
+    keys: ['username']
+}));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.listen (port, function() {
+app.listen(port, function () {
     console.log("In ascolto sulla porta " + port);
 });
